abfbc3b3c09e9fd7a1fd3b998e9c4000bb0264fa
Steven Dinhton Nguyen <stevendinhton@gmail.com>
2016-11-06 02:52:35 -0500

    API access complete
    Bot is not finished yet, but code for accessing the twitter api is
    complete. Using the API through a terminal is now possible.

--- bot.py
+++ bot.py
@@ -1,2 +1,18 @@
 import configparser
+import tweepy
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+# get keys and secrets from config.ini file
+CONSUMER_KEY = config['TWITTER']['CONSUMER_KEY']
+CONSUMER_SECRET = config['TWITTER']['CONSUMER_SECRET']
+ACCESS_KEY = config['TWITTER']['ACCESS_KEY']
+ACCESS_SECRET = config['TWITTER']['ACCESS_SECRET']
+
+# connect to Twitter API
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
+
 
